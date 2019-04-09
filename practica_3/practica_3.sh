2fdfa6e16c102da77872b25a45b91940e5079afa
hayk99 <haykk1999@gmail.com>
2019-04-09 18:56:18 +0200

    casi está

--- practica_3/practica_3.sh
+++ practica_3/practica_3.sh
@@ -2,10 +2,9 @@
 #Autores: Hayk Kocharyan (757715) y Jose Felix Yagüe (755416)
 #Practica 3 ADSIS
 
-root=$(sudo id -u)
+root=$(id -u)
 if [ $root = "0" ]
 then
-	usrexec=$(whoami)
 	#sudo sh -c "echo \"$usrexec	ALL=(ALL:ALL) ALL\" >> /etc/sudoers"
 	
 	#comprobar si hay 2 parámetros
@@ -17,19 +16,67 @@ then
 		then
 			while read line
 			do
-				id=$(echo $line| cut -d ',' -f1)
-				pass=$(echo $line| cut -d ',' -f2)
-				name=$(echo $line| cut -d ',' -f3)
-			if [ $id -gt 1815 ]
-				then 
-					sudo useradd -u $id -g $i d-f 30 -p $pass $name
+				#lee del fichero los campos
+				name=$(echo $line | cut -d ',' -f1)
+				pass=$(echo $line | cut -d ',' -f2)
+				longName=$(echo $line | cut -d ',' -f3)
+				#comprobamos que no sean campos vacios
+				if [ $name != "" && $pass != "" && $longName != "" ]
+				then
+					#compruebas que id sea > 1815 
+					#0 si existe 1 sino
+					#comprobar que no existe el nombre
+					id -u $name
+					if [ $? ]
+					then
+						sudo useradd -U -m -K UID_MIN=1815 -c $longName $name
+						echo "$name:$pass" | chpasswd
+						sudo passwd $name -x 30
+						echo "$longName ha sido creado"
+					elif [ !$kk1 ]
+					then 
+						echo "El usuario $name ya existe"
+					fi
+				else 
+					echo "Campo invalido"
+				fi
 			done < "$1"
 
 		elif [ $1 = "-s" ]
 		then 
-			sudo mkdir -p /extra/backup
-			
-			...
+			if [ ! -d "/extra/backup" ]
+			then
+				sudo mkdir -p /extra/backup
+			fi
+			while read line
+			do
+				name=$(echo $line | cut -d ',' -f1)
+				#compruebas que id sea > 1815
+				#0 si existe 1 sino
+				#comprobar que no existe id
+				id -u $name
+				if [ !$? ]
+				then
+					#borro
+					tar -cvzf /extra/backup/$name.tar.gz /home/$name
+					userdel 
+				elif [ $kk ]
+				then
+					echo "El usuario $ids no existe"
+				fi
+
+				#leemos solo el primer campo que es id
+				ids=$(echo $line | cut -d ',' -f1)
+				#comprobar que no existe usuario
+				if [ ! $(id -u $id) ]
+				then
+					tar
+					sudo userdel -r $name
+				else
+					echo "El usuario $ids ya existe"
+				fi
+			done < "$1"
+
 		else
 			echo -e "Opcion invalida.\n"
 		fi
