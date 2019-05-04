ba7f606ab9c2fba2e69c38c98313de1710834935
hayk99 <haykk1999@gmail.com>
2019-05-04 11:58:36 +0200

    4 done

--- practica_4/practica_4.sh
+++ practica_4/practica_4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Autores: Hayk Kocharyan (757715) y Jose Felix Yagüe (755416)
-#Practica 3 ADSIS
+#Practica 4 ADSIS
 
 root=$(id -u)
 if [ $root = "0" ]
@@ -11,84 +11,94 @@ then
 			#comprobar si es añadir o eliminar
 			if [ $1 = "-a" ]
 			then
-				while read lineIP <&4; do
-					echo "leido $lineIP"
+				#itero fichero de ips
+				while read lineIP <&3; do
+					#compruebo que sea un ip válida sintácticamente
 					if [[ $lineIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
 					then
-						#ssh -i ~/.ssh/id_as_ed25519 as@"$lineIP" echo prueba
-						#echo "saca esto $?"
-						if ssh -i ~/.ssh/id_as_ed25519 as@"$lineIP" echo prueba 
+						#hago ssh para ver si puedo realizar una conexion válida
+						if ssh -i ~/.ssh/id_as_ed25519 as@$lineIP echo  >/dev/null 2>&1 
 						then
-							while read line <&3;do
-								echo "ip leida $lineIP"
-								echo "linea leida $line"
-								#lee del fichero los campos
+							#itero fichero de usuarios a crear
+							while read line <&4;do
+								#lee del fichero los campos nombre, password, y nombre completo
 								name=$(echo $line | cut -d ',' -f1)
 								pass=$(echo $line | cut -d ',' -f2)
 								longName=$(echo $line | cut -d ',' -f3)
 								#comprobamos que no sean campos vacios
 								if [[ ( $name != "" ) && ( $pass != "" ) && ( $longName != "" ) ]]
 								then
-									#compruebas que id sea > 1815 
-									#0 si existe 1 sino
-									#comprobar que no existe el nombre
-									ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" "sudo id -u $name >/dev/null 2>&1"
+									#comprobar que no existe el nombre del usuario que quiero añadir
+									ssh -i ~/.ssh/id_as_ed25519 as@$lineIP "sudo id -u $name >/dev/null 2>&1"
 									if [ $? -ne 0 ]
 									then
-										ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " sudo useradd -U -m -K UID_MIN=1815 -k /etc/skel -c "$longName" "$name" 2>/dev/null "
-										ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " sudo echo "$name:$pass" | sudo chpasswd "
-										ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " sudo passwd -x 30 $name | 2>/dev/null "
-										ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " sudo usermod -U $name "
-										echo "$longName ha sido creado"
+										#añado suaurio
+										ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " sudo useradd -U -m -K UID_MIN=1815 -k /etc/skel -c "$longName" "$name" 2>/dev/null "
+										#ponemos password deseado 
+										ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " sudo echo "$name:$pass" | sudo chpasswd "
+										#añadimos fecha de expiracion del password
+										ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " sudo passwd -x 30 $name | 2>/dev/null "
+										ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " sudo usermod -U $name "
+										echo $longName ha sido creado
 									else
-										echo "El usuario $name ya existe"
+										echo El usuario $name ya existe
 									fi
 								else 
 									echo "Campo invalido"
 								fi
-							done 3< "$2"
+							done 4< "$2"
+						else echo $lineIP no es accesible
 						fi
-					else echo ip no valida 
+					else echo formato ip: $lineIP no valido
 					fi
-				done 4< "$3"
+				done 3< "$3"
 			elif [ $1 = "-s" ]
 			then 
-				while read lineIP; do
+				#itero fichero de ips
+				while read lineIP <&3; do
+					#compruebo que sea un ip válida sintácticamente
 					if [[ $lineIP =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]
 					then
-						if  ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " test -d "/extra/backup" "
+						#hago ssh para ver si puedo realizar una conexion válida
+						if ssh -i ~/.ssh/id_as_ed25519 as@$lineIP echo prueba 2>> /dev/null 
 						then
-							  ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " mkdir -p /extra/backup "
-						fi
-						while read line
-						do
-							name=$(echo $line | cut -d ',' -f1)
-							#compruebas que id sea > 1815
-							#0 si existe 1 sino
-							#comprobar que no existe id
-							  ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " id -u $name >/dev/null 2>&1 "
-							if [ $? -eq 0 ] 
+							# compruebo que tenga el directorio /extra/backup
+							if  ! ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " test -d /extra/backup "
 							then
-								#borro
-								  ssh -T -i ~/.ssh/id_as_ed25519 as@"$lineIP" " tar -czf /extra/backup/$name.tar /home/$name 2>/dev/null "
-								#devuelve 0 si es correcto
-								if [ !$? ] 
-								then
-										 ssh  -i ~/.ssh/id_as_ed25519 as@"$lineIP" " userdel -r $name 2>/dev/null "
-								fi
+								#como no lo tengo, lo creo
+								ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " sudo mkdir -p /extra/backup "
 							fi
-						done < "$2"
-					else echo ip incorrecta
+							#itero fichero de usuarios
+							while read line <&4 ; do
+								#cojo solo el nombre, lo demás no me interesa
+								name=$(echo $line | cut -d ',' -f1)
+								#comprobar que existe usuario que me dispongo a borrar
+								ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " sudo id -u $name >/dev/null 2>&1 "
+								if [ $? -eq 0 ] 
+								then
+									#hago la copia de sus archivos
+									ssh -i ~/.ssh/id_as_ed25519 as@$lineIP " sudo tar -czf /extra/backup/$name.tar /home/$name 2>/dev/null "
+									#devuelve 0 si es correcto
+									if [ !$? ] 
+									then
+										#borro usuario si he podido crear la copia
+										ssh -i ~/.ssh/id_as_ed25519  -i ~/.ssh/id_as_ed25519 as@"$lineIP" " sudo userdel -r $name 2>/dev/null "
+									fi
+								fi
+							done 4< "$2"
+						else echo $lineIP no es accesible
+						fi 
+					else echo formato ip: $lineIP incorrecta
 					fi
-				done < "$3"
+				done 3< "$3"
 			else 
-				echo -e "Opcion invalida.\n"
+				echo -e "Opcion invalida"
 			fi
 	else
-		echo -e "Numero de parametros incorrectos. \n"
+		echo -e "Numero de parametros incorrectos"
 	fi
 		
 else
-	echo -e "Este script necesita privilegios de administracion. \n"
+	echo -e "Este script necesita privilegios de administracion"
 	exit 1
 fi
--- practica_4/redirect.sh
+++
@@ -1,5 +0,0 @@
-
-for line in "$3"
-do
-	echo $line
-done
