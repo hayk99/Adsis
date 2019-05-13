382e3a03c7c749d490322b3ebf0e84cb529beb71
as <as@debian.debian>
2019-05-13 11:44:12 +0200

    done

--- practica_5/practica5_prarte4_lv.sh
+++ practica_5/practica5_prarte4_lv.sh
@@ -2,31 +2,33 @@
 #AUTORES: Hayk Kocharyan (757715) & Jose Felix Yagüe (755416)
 
 read grupo vol tam filsis dir basura
-while [ $(echo "$nombreGrupo" | wc -w)  -gt 0  ]; do
+while [ $(echo "$grupo" | wc -w)  -gt 0  ]; do
 	#escaneamos para ver que grupos tenemos
 	grupos=$(sudo vgscan)
 	#miramos si existe el grupo deseado en los escaneados anteriormente
-	echo "$grupos" | grep "$vg"
-	if [ $? -eq 1]
+	echo "$grupos" | grep "$grupo"
+	if [ $? -gt 0 ]
 	then  
-		echo "No existe grupo"$vg""
+		echo "No existe grupo"$grupo""
 		exit 1
 	fi
 	#escaneamo para ver que volumenes hay
 	logicos=$(sudo lvscan)
 	#miramos si existe el nuestro
 	echo "$logicos" | grep "$vol"
-	if [ $? -eq 1]
+	if [ $? -gt 0 ]
 	then
+		echo creo
 		#no existe, lo creo
 		sudo lvcreate -L $tam --name $vol $grupo
 		sudo mkfs -t $filsis /dev/$grupo/$vol
 		sudo mount -t $filsis /dev/$grupo/$vol $dir
-		echo "/dev/$grupo/$vol $dir $filsis default 0 2\n" | sudo tee -a /etc/fstab
+		echo "/dev/$grupo/$vol $dir $filsis default 0 2" | sudo tee -a /etc/fstab
 	else
+		echo aumento
 		#existe, aumento
 		sudo lvextend -L+$tam /dev/$grupo/$vol
-		#sudo e2fsck -f /dev/$grupo/$vol
+		sudo e2fsck -f /dev/$grupo/$vol
 		sudo resize2fs /dev/$grupo/$vol
 	fi
 	read grupo vol tam filsis dir basura
